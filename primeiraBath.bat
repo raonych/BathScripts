6d2e3ee17343855647aff3ae22755e406338ed9e
raonych <raonych@gmail.com>
2024-06-25 22:00:01 -0300

    Add files via upload

---
+++ primeiraBath.bat
@@ -0,0 +1,13 @@
+@echo off
+echo "Limpando a tela e aguardando para mostrar arquivos"
+pause
+cls
+pause
+echo "Mostrando aquivos"
+pause
+dir
+pause
+echo "Mostrando sua estrutura de pastas"
+pause
+tree
+pause
